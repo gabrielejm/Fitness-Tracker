061149a2feb6d38331b11c0bdc327629731e3f34
Justin Gabriele <gabrielejustin@yahoo.com>
2021-01-03 08:32:17 -0500

    required mongojs

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const mongojs = require("mongojs");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const db = mongojs(databaseUrl, collections);
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
