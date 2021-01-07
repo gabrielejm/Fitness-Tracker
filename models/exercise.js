e22ad05030d4a02911b113cafcf3176980d730c1
Justin Gabriele <gabrielejustin@yahoo.com>
2021-01-07 18:19:11 -0500

    updated exercise model

--- models/exercise.js
+++ models/exercise.js
@@ -2,12 +2,37 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ExerciseSchema = new Schema({
-  type: String,
-  name: String,
-  duration: Number,
-  weight: Number,
-  reps: Number,
-  sets: Number,
+  type: {
+    type: String,
+    trim: true,
+    required: "Enter an exercise type",
+  },
+  name: {
+    type: String,
+    trim: true,
+    required: "Enter the exercise name",
+  },
+  duration: {
+    type: Number,
+    trim: true,
+    required: "Enter the exercise duration",
+  },
+  weight: {
+    type: Number,
+    trim: true,
+  },
+  reps: {
+    type: Number,
+    trim: true,
+  },
+  sets: {
+    type: Number,
+    trim: true,
+  },
+  distance: {
+    type: Number,
+    trim: true,
+  },
 });
 
 const Exercise = mongoose.model("Exercise", ExerciseSchema);
