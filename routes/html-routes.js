8366efccd220c6c090fac41a9792bb4152ca7885
Justin Gabriele <gabrielejustin@yahoo.com>
2021-01-08 08:13:17 -0500

    server working again

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,1 +1,20 @@
 const router = require("express").Router();
+
+// var path = require("path");
+
+// module.exports = function(app) {
+//   app.get("/images/:id", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/detail.html"));
+//   });
+
+//   app.get("/favorites", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/favorites.html"));
+//   });
+
+//   app.get("*", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/index.html"));
+//   });
+
+// };
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,24 +2,25 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 const db = require("./models");
 const htmlRoutes = require("./routes/html-routes");
 const apiRoutes = require("./routes/api-routes");
 
-const app = express();
-
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(logger("dev"));
-app.use(express.static("public"));
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useFindAndModify: false });
 
 app.use(require("./routes/html-routes"));
 app.use(require("./routes/api-routes"));
+
+// require("./routes/api-routes")(app);
+// require("./routes/html-routes")(app);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useFindAndModify: false });
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
